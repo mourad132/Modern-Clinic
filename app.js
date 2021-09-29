9242b0e51be9ff0e6e8648e9d3102893fa7383e3
mourad132 <58851960+mourad132@users.noreply.github.com>
2021-09-29 17:38:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,152 +24,41 @@ app.use(function(req, res, next) {
     next();
   });
 
-//@route /cases 
-//@desc Retrieve All The Cases
-//@method GET
+// -----------------------
+// ***** Case Routes *****
+// -----------------------
 
-app.get('/delete/:id', (req, res) => {
-	CaseHistory.findOneAndDelete({_id: req.params.id}, (err, deleted) => {
-		if(err){
-			console.log(err)
-		}
-	})
-})
-
-app.get('/history', (req, res) => {
-	CaseHistory.find({}, (err, found) => {
-		if(err){
-			console.log(err)
-		} else {
-			res.send(found)
-		}
-	})
-})
-
-app.post("/history/new/:id", (req, res) => {
-	Case.find({_id: req.params.id}, (err, found) => {
-		if(err){
-			res.send(err)
-		} else {
-		CaseHistory.create({
-			name: found.name,
-			paid: found.paid,
-			assigned: found.assigned,
-			description: found.description,
-			date: found.date,
-			number: found.number,
-	}, (err, created) => {
-		if(err){
-			res.send(err)
-		} 
-	})
-		}
-	})
-})
-
+//get all cases
 app.get('/cases', (req, res) => {
+	// find all cases
 	Case.find({}, (err, newCases) => {
 		if(err){
+			//if there is an error
+			//send status code 500 (server error)
 			res.sendStatus(500)
+			//print it out
 			console.log(err)
 		} else {
+			// send the cases found
 			res.send(JSON.stringify(newCases))
 		}
 	})
 })
 
-app.get('/clearIncome', (req, res) => {
-	Income.findOne({}, (err, income) => {
+// Deleted Case By Id
+app.get('/delete/:id', (req, res) => {
+	// Find A Case A Delete it
+	CaseHistory.findOneAndDelete({_id: req.params.id}, (err, deleted) => {
 		if(err){
+			// if there is any error, print it out
 			console.log(err)
-		} else {
-			income.income = 0
-			income.save()
-		}
-	})
-})
-
-app.get('/clearExpense', (req, res) => {
-	totalExpense.findOne({}, (err, total) => {
-		if(err){
-			console.log(err)
-		} else {
-			total.total = 0
-			total.save()
-		}
-	})
-})
-//@route /case/new
-//@desc Creates A New Case
-//@method POST
-
-app.post('/case/new', (req, res) => {
-	Case.create({
-		name: req.body.name,
-		paid: req.body.paid,
-		assigned: req.body.assigned,
-		description: req.body.description,
-		date: req.body.date,
-	}, (err, created) => {
-		if(err){
-			console.log(err)
-		} else {
-			Income.findOne({}, (err, income) => {
-				if(err){
-					console.log(err)
-				} else {
-					income.income += created.paid
-					income.save()
-					res.send(income)
-				}
-			})
-			res.send(created)
-		}
-	})
-})
-
-//Income & Profit Route
-app.get("/income", (req, res) => {
-	Income.findOne({}, (err, income) => {
-		if(err){
-			console.log(err);
-		} else {
-			totalExpense.findOne({}, (err, total) => {
-				if(err){
-					console.log(err)
-				} else {
-					res.send({income: income, total: total});					
-				}
-			})
-		}
-	})
-});
-
-app.get('/expenses', (req, res) => {
-	Expense.find({}, (err, expenses) => {
-		if(err){
-			console.log(err)
-		} else {
-			res.send(JSON.stringify(expenses))
-		}
-	})
-})
-
-//Total Expense Function
-
-//Total Expenses Route
-app.get('/totalExpenses', (req, res) => {
-	totalExpense.findOne({}, (err, found) => {
-		if(err){
-			console.log(err)
-		} else {
-			res.send(found)
 		}
 	})
 })
 
 //New Case Route 
 app.post('/new/case', (req, res) => {
+	//create new case
 	Case.create({
 		name: req.body.name,
 		paid: req.body.paid,
@@ -179,14 +68,19 @@ app.post('/new/case', (req, res) => {
 		done: false,
 	}, (err, newCase) => {
 		if(err){
+			//if there is an error, print it out
 			console.log(err)
 		} else {
+			//find income
 			Income.findOne({}, (err, income) => {
 				if(err){
 					console.log(err)
 				} else {
+					//increment it by the new case
 					income.income += newCase.paid
+					//save the income 
 					income.save()
+					// send it
 					res.send(income)
 				}
 			})
@@ -194,35 +88,59 @@ app.post('/new/case', (req, res) => {
 	})
 })
 
-//New Expense Route
-app.post('/new/expense', (req, res) => {
-	Expense.create({
+
+//Update Case Route
+app.put('/case/update', (req, res) => {
+	//finds a case and updates it
+	Case.findOneAndUpdate(req.body.id, {
 		name: req.body.name,
+		paid: req.body.paid,
+		assigned: req.body.assigned,
 		description: req.body.description,
-		price: req.body.price
-	}, (err, expenses) => {
+		date: req.body.date,
+		number: req.body.number,
+	}, (err, newCase) => {
+		//if there is an error
 		if(err){
+			//print it out
 			console.log(err)
 		} else {
-			totalExpense.findOne({}, (err, total) => {
-				if(err){
-					console.log(err)
-				} else {
-					total.total += expenses.price
-					total.save()
-				}
-			})
+			//send the new case
+			res.send(newCase)
 		}
-	}
-	)
+	})
 })
+
+
+// ----------------------
+// *** History Routes ***
+// ----------------------
+
+// Sends Case History (Recently Deleted Cases)
+app.get('/history', (req, res) => {
+	//finds all cases 
+	CaseHistory.find({}, (err, found) => {
+		if(err){
+			// if there is any error, print it out
+			console.log(err)
+		} else {
+			// and send them
+			res.send(found)
+		}
+	})
+})
+
 
 //Move To History Case Route
 app.get('/case/delete/:id', (req, res) => {
+	//find case using id
 	Case.findById(req.params.id, (err, found) => {
+		//if there is an error
 		if(err){
+			//print it out
 			console.log(err)
 		} else {
+			//create new recently deleted case
 			CaseHistory.create({
 				name: found.name,
 				paid: found.paid,
@@ -230,13 +148,19 @@ app.get('/case/delete/:id', (req, res) => {
 				date: found.date,
 				assigned: found.assigned,
 			}, (err, history) => {
+				// if there is an error
 				if(err){
+					//print it out
 					console.log(err)
 				} else {
+					//find the case and delete it
 					Case.findOneAndDelete({_id: found._id}, (err, deleted) => {
+						//if there is an error 
 						if(err){
+							//print it out
 							console.log(err)
 						} else {
+							//send the recently deleted case
 							res.send(history)
 						}
 					})
@@ -246,35 +170,82 @@ app.get('/case/delete/:id', (req, res) => {
 	})
 });
 
-//Update Case Route
-app.put('/case/update', (req, res) => {
-	Case.findOneAndUpdate(req.body.id, {
-		name: req.body.name,
-		paid: req.body.paid,
-		assigned: req.body.assigned,
-		description: req.body.description,
-		date: req.body.date,
-		number: req.body.number,
-	}, (err, newCase) => {
+// Add New Case To Recently Deleted Cases
+app.post("/history/new/:id", (req, res) => {
+	// find the case
+	Case.find({_id: req.params.id}, (err, found) => {
 		if(err){
+			// if there is an error, send it
+			res.send(err)
+		} else {
+			// create new case history from the deleted case
+			CaseHistory.create({
+				name: found.name,
+				paid: found.paid,
+				assigned: found.assigned,
+				description: found.description,
+				date: found.date,
+				number: found.number,
+		}, (err, created) => {
+			if(err){
+				// if there is any error, send it
+				res.send(err)
+			} 
+		})
+			}
+		})
+})
+
+// ---------------------
+// ** Expenses Routes **
+// ---------------------
+
+// clear Total Expenses
+app.get('/clearExpense', (req, res) => {
+	//find the total expenses
+	totalExpense.findOne({}, (err, total) => {
+		if(err){
+			// if there is an error, print it out
 			console.log(err)
 		} else {
-			res.send(newCase)
+			//make total = 0
+			total.total = 0
+			//save it
+			total.save()
+		}
+	})
+})
+
+// expenses route
+app.get('/expenses', (req, res) => {
+	//find all expenses
+	Expense.find({}, (err, expenses) => {
+		if(err){
+			//if there is an error, print it out
+			console.log(err)
+		} else {
+			// send the expenses 
+			res.send(JSON.stringify(expenses))
 		}
 	})
 })
 
 //Update Expense Route
 app.put('/expense/update', (req, res) => {
+	//initiate id
 	var id = req.body.id
+	//find the expense and update it
 	Expense.findOneAndUpdate(id, {
 		name: req.body.name,
 		description: req.body.description,
 		price: req.body.price
 	}, (err, updated) => {
+		//if there is an error
 		if(err){
+			//print it out
 			console.log(err)
 		} else {
+			//send the updated one
 			res.send(JSON.stringify(updated))
 		}
 	})
@@ -282,16 +253,24 @@ app.put('/expense/update', (req, res) => {
 
 //Delete Expense
 app.get('/expense/delete/:id', (req, res) => {
+	//find an expense and delete it
 	Expense.findOneAndDelete(req.params.id, (err, expense) => {
+		//if there is an error
 		if(err){
+			//print it out
 			console.log(err)
 		}
 		else {
+			// find the total expense
 			totalExpense.findOne({}, (err, total) => {
+				// if there is an error
 				if(err){
+					// print it out
 					console.log(err)
 				} else {
+					// decrement the expense price from total
 					expense.price -= total.total
+					//save total
 					total.save()
 				}
 			})
@@ -299,7 +278,101 @@ app.get('/expense/delete/:id', (req, res) => {
 	})
 })
 
+
+//Total Expense Function
+
+//Total Expenses Route
+app.get('/totalExpenses', (req, res) => {
+	//find total expense
+	totalExpense.findOne({}, (err, found) => {
+		if(err){
+			//if there is an error, print it out
+			console.log(err)
+		} else {
+			// send it
+			res.send(found)
+		}
+	})
+})
+
+//New Expense Route
+app.post('/new/expense', (req, res) => {
+	//create new expense
+	Expense.create({
+		name: req.body.name,
+		description: req.body.description,
+		price: req.body.price
+	}, (err, expenses) => {
+		// if there is an error
+		if(err){
+			//print it out
+			console.log(err)
+		} else {
+			//find total expense
+			totalExpense.findOne({}, (err, total) => {
+				//if there is an error
+				if(err){
+					// Print it out
+					console.log(err)
+				} else {
+					//increment total by the new expense
+					total.total += expenses.price
+					//save the total
+					total.save()
+				}
+			})
+		}
+	}
+	)
+})
+
+// ------------------------------
+// *** Income & Profit Routes ***
+// ------------------------------
+
+// clears the income
+app.get('/clearIncome', (req, res) => {
+	//find the income 
+	Income.findOne({}, (err, income) => {
+		if(err){
+			// if there is an error, print it out
+			console.log(err)
+		} else {
+			//clear it
+			income.income = 0
+			//save it
+			income.save()
+		}
+	})
+})
+
+//Income & Profit Route
+app.get("/income", (req, res) => {
+	//find income
+	Income.findOne({}, (err, income) => {
+		if(err){
+			//if there is an error, print it out
+			console.log(err);
+		} else {
+			//find total expense
+			totalExpense.findOne({}, (err, total) => {
+				if(err){
+					// if there is an error, print it out
+					console.log(err)
+				} else {
+					//send total income and expense
+					res.send({income: income, total: total});					
+				}
+			})
+		}
+	})
+});
+
+// --------------------
+// *** Start Server ***
+// --------------------
+
+// listen for server
 app.listen(process.env.PORT || 80, function(){
     console.log('server started')
 })
-
